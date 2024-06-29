44c3a1d9b9ca1da927f648fe43659581a7890160
otomdee <otomdaniel65@gmail.com>
2024-06-29 11:11:43 +0100

    add play round function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+computerScore = 0;
+userScore = 0;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 9);
     let randChoice = choice % 3;
@@ -22,4 +26,44 @@ function getUserChoice() {
     }
 }
 
-console.log(getUserChoice());
+function playRound() {
+    const computerChoice = getComputerChoice();
+    const humanChoice = getUserChoice();
+
+    if (computerChoice == "Rock") {
+        if humanChoice == "Rock" {
+            return ("draw")
+        }
+        else if (humanChoice == "Paper") {
+            return("loss")
+        }
+        else {
+            return ("win")
+        }
+    }
+
+    if (computerChoice == "Paper") {
+        if humanChoice == "Rock" {
+            return ("win")
+        }
+        else if (humanChoice == "Paper") {
+            return("draw")
+        }
+        else {
+            return ("loss")
+        }
+    }
+
+    if (computerChoice == "Scissors") {
+        if humanChoice == "Rock" {
+            return ("loss")
+        }
+        else if (humanChoice == "Paper") {
+            return("win")
+        }
+        else {
+            return ("draw")
+        }
+    }
+
+}
